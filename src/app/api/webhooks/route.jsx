3ab24b4053192cd191a401c8cf1d7f26c4cf3ddb
Roshan <chawalroshan@gmail.com>
2025-03-05 19:39:03 +0545

    fix the error not importing

--- src/app/api/webhooks/route.jsx
+++ src/app/api/webhooks/route.jsx
@@ -1,6 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { createOrUpdateUser, deleteUser } from '../../../lib/mongodb/user'
 
 export async function POST(req) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET
